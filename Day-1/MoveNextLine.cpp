882d00651258023548683cd43935b9dd8403f1e4
VIRENDRA-18 <virusingh0125@gmail.com>
2025-08-05 12:21:26 +0530

    Learn Variable and how to move next line

---
+++ Day-1/MoveNextLine.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Virendra!"<<endl;
+    cout<<"Hii Kumkum! \n";
+    cout<<"I am learning C++ programming.";
+    return 0;
+}
---
+++ Day-1/VariableDeclear.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5; // Declare an integer variable
+    a = 9; // Assign a new value to the variable
+    // Output the value of the variable
+
+    float b = 3.14; // Declare a float variable
+    cout<<a + b;
+    return 0;
+}
